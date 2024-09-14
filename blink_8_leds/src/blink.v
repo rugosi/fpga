8c2aebbc6920817e8ac6392b70ef12f26024f615
rugosi <kukacster@gmail.com>
2024-09-14 21:18:58 +0200

    moving leds split to .ccf instead of .v

--- blink_8_leds/src/blink.v
+++ blink_8_leds/src/blink.v
@@ -4,32 +4,9 @@ module blink(
 		input wire clk,
 		input wire rst,
 		output wire led,
-		output wire led0,
-		output wire led1,
-		output wire led2,
-		output wire led3,
-		output wire led4,
-		output wire led5,
-		output wire led6,
-		output wire led7,
-
-		output wire pmod_led1,
-		output wire pmod_led2,
-		output wire pmod_led3,
-		output wire pmod_led4,
-		output wire pmod_led5,
-		output wire pmod_led6,
-		output wire pmod_led7,
-		output wire pmod_led8,
-
-		output wire rgb_led0,
-		output wire rgb_led1,
-		output wire rgb_led2,
-		output wire rgb_led3,
-		output wire rgb_led4,
-		output wire rgb_led5,
-		output wire rgb_led6,
-		output wire rgb_led7,
+		output wire [7:0] leds,
+		output wire [7:0] pmod_leds,
+		output wire [7:0] rgb_leds,
 
 		output wire rgb_r,
 		output wire rgb_b,
@@ -58,37 +35,15 @@ module blink(
 
 	assign led = counter[27];
 
-	assign led0 = sum[0];
-	assign led1 = sum[1];
-	assign led2 = sum[2];
-	assign led3 = sum[3];
-	assign led4 = sum[4];
-	assign led5 = sum[5];
-	assign led6 = sum[6];
-	assign led7 = sum[7];
+	assign leds = sum;
 
-	assign pmod_led1 = sum[0];
-	assign pmod_led2 = sum[1];
-	assign pmod_led3 = sum[2];
-	assign pmod_led4 = sum[3];
-	assign pmod_led5 = sum[4];
-	assign pmod_led6 = sum[5];
-	assign pmod_led7 = sum[6];
-	assign pmod_led8 = sum[7];
+	assign pmod_leds = sum;
 
     assign rgb_r = color == 2'd1 || color == 2'd0 ? 0 : 1;
     assign rgb_g = color == 2'd2 || color == 2'd0 ? 0 : 1;
     assign rgb_b = color == 2'd3 || color == 2'd0 ? 0 : 1;
 
-	assign rgb_led0 = sum[0]; 
-	assign rgb_led1 = sum[1]; 
-	assign rgb_led2 = sum[2];
-	assign rgb_led3 = sum[3];
-	assign rgb_led4 = sum[4];
-	assign rgb_led5 = sum[5];
-	assign rgb_led6 = sum[6];
-	assign rgb_led7 = sum[7];
-
+	assign rgb_leds = sum;
 
 	always @(posedge clk0)
 	begin
